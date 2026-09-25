45c8965433ac7a98be491901c019806ab4901d40
Saransh-Patil <146372094+Saransh-patil@users.noreply.github.com>
2026-09-25 14:29:07 +0530

    Time: 9 ms (67.57%), Space: 12.4 MB (26.14%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -4,20 +4,8 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        dictionary={}
-        y=90
-        x=122
-        for i in range(1,27):
-            m=str(chr(x))
-            n=str(chr(y))
-            dictionary[m]=i
-            dictionary[n]=i
-            y-=1
-            x-=1
-        sum=0
-        for i in range(len(s)):
-            if s[i] in dictionary:
-                sum+=dictionary[s[i]]*(i+1)
+        sum = 0
+        for i in range(len(s)) :
+            sum += (i+1) * (26 - (ord(s[i])-ord('a')))
         return sum
-
         
